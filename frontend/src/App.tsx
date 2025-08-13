4720d7f3b518567273481df66532e41c9d67b4f3
Ruwini Tharanga <12345tharanga12345@gmail.com>
2025-08-13 13:03:11 +0530

    Dashboard added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Dashboard from "./pages/Dashboard";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <>
-  Welcome to Task Manager!
-  </>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
